2a2253e69f962014122b927c8ece3cc69a150af9
matdid <mattias.didriksson@lm.se>
2022-12-09 11:01:50 +0100

    USCH.

--- dag8/day8.hs
+++ dag8/day8.hs
@@ -4,19 +4,23 @@ import Data.Char ( digitToInt )
 
 data Tree = Tree {
         height :: Int,
-        seen :: Bool
+        seen :: Bool,
+        visibilityScore :: Int
     }  deriving(Show, Eq)
 
 
 parseTree :: Char -> Tree
-parseTree x = Tree num False
+parseTree x = Tree num False (-1)
     where num = digitToInt x
 
 parseRow :: [Char] -> [Tree]
 parseRow r = map parseTree r
 
 markSeen :: Tree -> Tree
-markSeen t = Tree (height t) True
+markSeen t = Tree (height t) True 0
+
+scoreForTree :: Tree -> Int -> Tree
+scoreForTree tree score = Tree (height tree) (seen tree) score
 
 seenTrees :: Int -> [Tree] -> [Tree] -> [Tree]
 seenTrees prevHigh seen [] = seen
@@ -26,16 +30,43 @@ seenTrees prevHigh seen (x:xs) =
             then seenTrees (height x) (seen ++ [markSeen x]) xs
         else seenTrees prevHigh (seen ++ [x]) xs
 
+
+visibilityForTree:: [Tree] -> [Tree] -> [Tree]
+visibilityForTree seen [] = seen
+visibilityForTree seen [a] = seen ++ [scoreForTree a 0]
+visibilityForTree  seen (x:xs) =
+    let currentH = height x
+        visibilityS = min (length xs) (1 + length (takeWhile (\t -> currentH > height t) xs))
+        newVisibilityscore = if visibilityScore x == -1
+                             then
+                                visibilityS
+                             else
+                                visibilityS * visibilityScore x
+        updatedX = scoreForTree x newVisibilityscore
+    in
+        visibilityForTree (seen ++ [updatedX]) xs
+
+
+performOnForest :: ([Tree] -> [Tree]) -> [[Tree]] -> [[Tree]]
+performOnForest op forest =
+    let
+        left = map op forest
+        top = map op $ transpose left
+        bottom = map (op . reverse) top
+        right = map (op . reverse) (transpose $ map reverse bottom)
+        done = map reverse right
+    in
+        done
+
+
 main :: IO ()
 main = do
     ls <- fmap lines (readFile "input.txt")
     let
-        left = map (seenTrees (-1) [] . parseRow) ls
-        top = map (seenTrees (-1) []) $ transpose left
-        bottom = map (seenTrees (-1) [] . reverse) top
-        right = map (seenTrees (-1) [] . reverse) (transpose $ map reverse bottom)
-        done = map reverse right
+        partA = performOnForest (seenTrees (-1) []) $ map parseRow ls
+        partB = performOnForest (visibilityForTree []) $ map parseRow ls
         in do
-            print $ length $ filter (==True) $ concatMap (map seen) done
+            print $ length $ filter (==True) $ concatMap (map seen) partA
+            print $ maximum $ concatMap (map visibilityScore) partB
 
 
