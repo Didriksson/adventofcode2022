5d0f8fb725991b1de688397bec97c49c0c97a181
Mattias Didriksson <mattias.didriksson@gmail.com>
2022-12-20 14:59:37 +0100

    3199 sekunder. Den här skulle må bra av lite optimering :D

--- dag14/day14.hs
+++ dag14/day14.hs
@@ -1,5 +1,6 @@
 import Control.Monad
 import Data.Char
+import Data.Maybe
 import Data.List
 import Data.List.Split (divvy)
 import Data.Either
@@ -11,8 +12,6 @@ import Text.ParserCombinators.Parsec
 type Coordinate = (Int, Int)
 
 data Board =  Board {
-  minX :: Int,
-  minY :: Int,
   maxX :: Int,
   maxY :: Int
 } deriving(Show)
@@ -20,7 +19,8 @@ data Board =  Board {
 data Cave = Cave {
   rocks :: [Coordinate],
   sand  :: [Coordinate],
-  boundBox :: Board
+  boundBox :: Board,
+  caveFloor :: Int
 } deriving (Show)
 
 pathParser :: GenParser Char st [[Coordinate]]
@@ -41,15 +41,13 @@ rangeFor v1 v2 = if v1 > v2
 
 expandRange c1 c2 = [ (x,y) | x <- rangeFor (fst c1) (fst c2), y <- rangeFor (snd c1) (snd c2)]
 
-reachedInifinity (x,y) (Board minX minY maxX maxY)
-  | x < minX = True
-  | y < minY = True
+reachedInifinity (x,y) (Board maxX maxY)
   | x > maxX = True
   | y > maxY = True
   | otherwise = False
 
 
-createBoundBox paths = Board (minimum xs) (minimum ys) (maximum xs) (maximum ys)
+createBoundBox paths = Board (maximum xs) (maximum ys)
     where
       pathPoints = (500,0) : concat (concat paths)
       xs =  map fst pathPoints
@@ -57,15 +55,46 @@ createBoundBox paths = Board (minimum xs) (minimum ys) (maximum xs) (maximum ys)
 
 expandPaths p = map (\(c1:c2:_) -> expandRange c1 c2) $ divvy 2 1 p
 
-dropSand (Cave rocks sand box) =
-   let pos = last takeWhile (\f -> f `elem` occupiedPositions) dropCoordinates
-    
+dropSand (Cave rocks sand box caveFloor) sandDropPosition =
+  case takeWhile (`notElem` occupiedPositions) dropCoordinates of
+    [] -> Nothing
+    potential -> Just $ last potential
+  where
+    boundedY = maxY box
+    dropCoordinates = [(x,y) | x <- [fst sandDropPosition], y <- [(snd sandDropPosition)..boundedY]]
+    occupiedPositions = rocks ++ sand ++ filter (\c -> snd c == caveFloor) dropCoordinates
 
-  where 
-    sandDropPosition = (500,0)
-    occupiedPositions = rocks ++ sand
-    maxY = maxY box
-    dropCoordinates = [(x,y) | x <- [500], y <- [(snd sandDropPosition)..maxY]]
+isFree occupied potential caveFloor = potential `notElem` occupied && snd potential /= caveFloor
+lowerLeft coord = (fst coord - 1, snd coord +1)
+lowerRight coord = (fst coord + 1, snd coord +1)
+
+evaluateAndDrop :: Cave -> Coordinate -> Maybe Coordinate
+evaluateAndDrop cave dropCoord =
+    let
+      dropped = dropSand cave dropCoord
+      occupiedPositions = rocks cave ++ sand cave
+    in
+      case dropped of
+        Nothing -> Nothing
+        Just coord ->
+            if isFree occupiedPositions (lowerLeft coord) (caveFloor cave)
+              then evaluateAndDrop cave (lowerLeft coord)
+            else if isFree occupiedPositions (lowerRight coord) (caveFloor cave)
+              then evaluateAndDrop cave (lowerRight coord)
+            else
+              dropped
+
+
+-- Drop from 500
+-- Do until come to rest
+dropUntilInfinity (Cave rockblocks sandblocks box cavefloor) =
+    let sandDropPosition = (500,0)
+        atRestCoord = evaluateAndDrop (Cave rockblocks sandblocks box cavefloor) sandDropPosition
+    in
+        case atRestCoord of
+          Just coord -> trace("At rest: " ++ show coord ++ " round: " ++ show (length sandblocks)) dropUntilInfinity (Cave rockblocks (coord:sandblocks) box cavefloor)
+          Nothing -> Cave rockblocks sandblocks box cavefloor
+
 
 main :: IO ()
 main = do
@@ -73,5 +102,6 @@ main = do
   let pathInput = fromRight [] $ parse pathParser "" ls
   let expandedPaths = map expandPaths pathInput
   let box = createBoundBox expandedPaths
-  let initState = Cave (concat $ concat expandedPaths) [] box
-  print $ initState
+  let initState = Cave (concat $ concat expandedPaths) [] box 11
+  let initStateB = Cave (concat $ concat expandedPaths) [] (Board (maxX box) (maxY box + 2)) (maxY box + 2)
+  print $ length $ sand $ dropUntilInfinity initStateB
